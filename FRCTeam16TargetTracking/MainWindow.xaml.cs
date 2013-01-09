f6a040769fd283eb7dd87de9a2125a743f2ac87a
Aaron Sanders <atsanders@gmail.com>
2013-01-08 22:50:10 -0600

    initial commit

---
+++ FRCTeam16TargetTracking/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Emgu.CV;
+using Microsoft.Kinect;
+using System.Windows.Media;
+using Emgu.CV.Structure;
+
+namespace FRCTeam16TargetTracking
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool _showPlayer = false;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
+
+
+        }
+
+        void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+            var oldSensor = (KinectSensor)e.OldValue;
+            if (oldSensor != null)
+            {
+                oldSensor.Stop();
+                oldSensor.AudioSource.Stop();
+            }
+
+            var sensor = (KinectSensor)e.NewValue;
+            if (sensor == null)
+            {
+                return;
+            }
+
+            sensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+            sensor.SkeletonStream.Enable(); //required to see players
+            sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(newSensor_AllFramesReady);
+
+            try
+            {
+                sensor.Start();
+            }
+            catch (System.IO.IOException)
+            {
+                kinectSensorChooser1.AppConflictOccurred();
+            }
+        }
+
+        void newSensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame == null)
+                {
+                    return;
+                }
+
+                byte[] pixels = GenerateColoredBytes(depthFrame);
+
+                //number of bytes per row width * 4 (B,G,R,Empty)
+                int stride = depthFrame.Width * 4;
+
+                image1.Source = System.Windows.Media.Imaging.BitmapSource.Create(depthFrame.Width, depthFrame.Height, 96, 96, PixelFormats.Bgr32, null, pixels, stride);
+
+                //testing
+                //Image<Gray, ushort> img = new Image<Gray, ushort>(640, 480, 640 * 2, depthFrame.); 
+
+                
+            }
+        }
+
+        private byte[] GenerateColoredBytes(DepthImageFrame depthFrame)
+        {
+
+            //get the raw data from kinect with the depth for every pixel
+            short[] rawDepthData = new short[depthFrame.PixelDataLength];
+            depthFrame.CopyPixelDataTo(rawDepthData);
+
+            //use depthFrame to create the image to display on-screen
+            //depthFrame contains color information for all pixels in image
+            //Height x Width x 4 (Red, Green, Blue, empty byte)
+            Byte[] pixels = new byte[depthFrame.Height * depthFrame.Width * 4];
+
+            //hardcoded locations to Blue, Green, Red (BGR) index positions       
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+
+            for (int depthIndex = 0, colorIndex = 0; depthIndex < rawDepthData.Length && colorIndex < pixels.Length; depthIndex++, colorIndex += 4)
+            {
+                //get the player (requires skeleton tracking enabled for values)
+                int player = rawDepthData[depthIndex] & DepthImageFrame.PlayerIndexBitmask;
+
+                //gets the depth value
+                int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth; //shift bits to right by 3
+
+                if (MMToFeet(depth) <= 3)
+                {
+                    pixels[colorIndex + BlueIndex] = 255;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+
+                }
+                else if (MMToFeet(depth) > 3 && MMToFeet(depth) <= 6)
+                {
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 255;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+                else if (MMToFeet(depth) > 6 && MMToFeet(depth) <= 10)
+                {
+                    
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 255;
+                }
+
+                else if (MMToFeet(depth) > 10 && MMToFeet(depth) <= 13)
+                {
+
+                    pixels[colorIndex + BlueIndex] = 255;
+                    pixels[colorIndex + GreenIndex] = 255;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+
+                //Color all players "gold"
+                if (player > 0 && _showPlayer)
+                {
+                    pixels[colorIndex + BlueIndex] = Colors.Gold.B;
+                    pixels[colorIndex + GreenIndex] = Colors.Gold.G;
+                    pixels[colorIndex + RedIndex] = Colors.Gold.R;
+                }
+
+            }
+
+
+            return pixels;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StopKinect(kinectSensorChooser1.Kinect);
+        }
+
+        private void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                if (sensor.IsRunning)
+                {
+                    sensor.Stop();
+
+                    if (sensor.AudioSource != null)
+                    {
+                        sensor.AudioSource.Stop();
+                    }
+                }
+            }
+        }
+
+        private double MMToFeet(int mm)
+        {
+            return mm * 0.0032808;
+        }
+
+        private void chkShowPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            _showPlayer = (bool)chkShowPlayer.IsChecked;
+        }
+
+    }
+}
